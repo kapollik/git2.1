740df20edcae139c885c1275ee91240842030a21
kapollik <kapollik@yandex.ru>
2018-12-20 14:18:00 +0300

    .

--- git2.1/Form1.Designer.cs
+++ git2.1/Form1.Designer.cs
@@ -40,7 +40,15 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -105,13 +113,13 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Location = new System.Drawing.Point(52, 36);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(598, 370);
+            this.panel1.Size = new System.Drawing.Size(717, 370);
             this.panel1.TabIndex = 7;
             // 
             // label4
@@ -131,6 +139,7 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "Войти";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
@@ -141,16 +150,75 @@
             // 
             // panel2
             // 
-            this.panel2.Location = new System.Drawing.Point(248, 199);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Location = new System.Drawing.Point(504, 36);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.Size = new System.Drawing.Size(265, 367);
             this.panel2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(56, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 48);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Мои бронирования";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(58, 134);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 48);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Мой профиль";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button7);
+            this.panel3.Controls.Add(this.button6);
+            this.panel3.Controls.Add(this.button5);
+            this.panel3.Location = new System.Drawing.Point(443, 27);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(262, 337);
+            this.panel3.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(53, 60);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(159, 48);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Обновить каталог";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(52, 144);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(159, 48);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Заказы по клиентам";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(52, 228);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(159, 48);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "Мой профиль";
+            this.button7.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
@@ -161,8 +229,11 @@
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -182,6 +253,12 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- git2.1/Form1.cs
+++ git2.1/Form1.cs
@@ -16,5 +16,31 @@ namespace git2._1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            panel2.Visible = false;
+            panel3.Visible = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Профиль еще не создан");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text=="admin")
+            {
+                panel1.Visible = false;
+                panel3.Visible = true;
+            }
+            else
+            {
+                panel1.Visible = false;
+                panel2.Visible = true;
+            }
+        }
     }
 }
